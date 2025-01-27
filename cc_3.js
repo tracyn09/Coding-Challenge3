ce4d19436f4244c790fccf04860e04c50a2bc940
tracyn09 <tracyn0099@gmail.com>
2025-01-27 14:35:26 -0500

    Task 1– Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,14 @@
+//Task 1
+
+//Declaring Price Array
+let prices = [23,35,46,33,29];
+
+//Adding New Price to Array
+prices.push(38);
+
+//Removing First Price from Arrat
+prices.shift()
+
+//Updated Price Array
+console.log(prices); 
+
