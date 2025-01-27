68bf5d6ef1b5a30d0c8f2a649a1b19d919da240c
tracyn09 <tracyn0099@gmail.com>
2025-01-27 14:45:17 -0500

    Task 2– Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -10,5 +10,19 @@ prices.push(38);
 prices.shift()
 
 //Updated Price Array
-console.log(prices); 
+console.log("Updated Prices List:", prices); 
 
+//Task 2
+
+//Declaring Order Array
+let orders = [37,56,43,22,67];
+
+//Increasing Third Order Quantity by 5
+orders[2] += 5; 
+
+//Total Number of Orders
+let totalorders= orders.reduce((sum,order)=> sum + order,0);
+
+//Updated Order Array
+console.log("Updated Orders List:", orders)
+console.log("Total Order Count:", totalorders);
